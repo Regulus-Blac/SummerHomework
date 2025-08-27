b71f8abf1fac89e1040b45ba9741000fb48924a5
Regulus-Blac <regulus367@outlook.com>
2025-08-27 20:00:03 +0800

    origin version

---
+++ func.h
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <windows.h>
+#include <stack> 
+
+using namespace std;
+#define INFEASIBLE -1
+#define TRUE 1
+#define FALSE 0
+#define MAX_VAR 5210
+#define FILE_MAX 30
+#define SUDOKU_VAR 729
+#define N 9
+#define MAX_BACK 730
+typedef int status;
+
+/*数据结构*/
+typedef struct literal {
+    int value; // 文字的值
+    struct literal *next; // 指向下一个文字的指针
+} LITERAL;
+
+typedef struct clause {
+    LITERAL *lit; // 子句中的文字
+    int num;     // 子句中文字的数量
+    bool isTrue; // 子句的真假值
+    struct clause *next; // 指向下一个子句的指针
+} CLAUSE;
+
+typedef struct cnf {
+    int num_clau; // 子句数量
+    int num_var;  // 变量数量
+    CLAUSE *head; // 指向子句链表的头
+    CLAUSE *tail; // 指向子句链表的尾
+} CNF;
+
+typedef struct info_clau{
+	CLAUSE *pre;
+	CLAUSE *self;
+} INFO_C;
+
+typedef struct info_lit{
+	CLAUSE *clause;
+	struct info_lit *next;
+} INFO_L;
+
+typedef struct {
+	int ans;
+	INFO_L *pos;		//正 
+	INFO_L *nag;		//负 
+} LITSHEET; 
+
+
+
+
+
+
+void initCNF(CNF &S, int var); 
+void clearCNF(CNF &S, stack <INFO_C> &op_clau,LITSHEET* ans);
+void createClause(CNF &S);
+bool buildCNF(CNF &S, int num_clau, FILE *fp, LITSHEET* ans);
+void buildLitsheet(LITSHEET* ans, CLAUSE *node, int value);
+
+LITSHEET* CNFparser(CNF &S, char file[],LITSHEET* ans);
+void showCNF(CNF &S);
+void showCNF(CNF &S,FILE *test);
+void showLitsheet(LITSHEET* ans, int range);
+void showClause(CLAUSE *head);
+
+bool isUnitClause(CLAUSE *clause);
+CLAUSE *existUnitClause(CLAUSE *head);
+bool existEmptyClause(CLAUSE *head); 
+CLAUSE *locatePre(CLAUSE *node,CLAUSE *head); 
+int deleteOneClause(CLAUSE *node, stack <INFO_C> &op_clau, CNF &S, LITSHEET* ans);
+
+int deleteClause(stack <INFO_C> &op_clau, CNF &S, LITSHEET* ans, int backtrace[]/*,FILE *test*/);
+int deleteLit( stack <INFO_C> &op_clau, CNF &S, int value, LITSHEET* ans/*,FILE *test*/);
+status restore_cl(int num, stack <INFO_C> &op_clau, CNF &S);
+status restore_lit(int back[], LITSHEET* ans);
+int choose_lit(CLAUSE *head, LITSHEET* ans);
+
+bool DPLL(stack <INFO_C> &op_clau, CNF &S, LITSHEET* ans/*,FILE *test*/);
+void check(LITSHEET* ans, int cnt);
+void check(FILE *test,LITSHEET* ans,int cnt);
+status saveOutput(LITSHEET* ans, int cnt, char file[], double used_time);
+    // 假设loadClause函数的实现是将当前子句添加到CNF中
+    // 这里需要根据具体的逻辑来实现
+    // 例如，可能需要将当前子句从输入中读取并添加到CNF中
+    // 但由于没有提供具体的实现细节，这里仅作为占位符
+    
+    
+    
+//void recoverCNF(CNF &S, stack <INFO_C> &op_clau,int ans[]);   
+//bool fundConsCNF(CNF &S);
+//bool percentConsCNF(CNF &S);
+//
+//int ijk_cnf(int ijk);
+//int cnf_ijk(int cnf);
+//int randomNum(int code);
+//
+//CLAUSE *addUnitClause(CNF &S, int value);
+//bool deleteS_tail(CNF &S, CLAUSE *base);
+//
+//bool DFS_board(/*FILE *test,*/int row, int col, int cnt,  stack <INFO_C> &op_clau, CNF &S, int board[N][N],int ans[]);
+//bool generate(/*FILE *test,*/ stack <INFO_C> &op_clau, CNF &S, int board[N][N], int ans[]);
+//
+//
+//void showBoard(int board[N][N]);
---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include "func.h"
+ 
+int main()
+{
+	clock_t start, end;
+	double used_time;
+	srand(time(NULL));
+	const char* inSuf = ".cnf";
+	/*FILE *test = fopen("000.txt","w");*/
+	stack <INFO_C> op_clau;
+	CNF S;  initCNF(S, 0);
+	CNF sudoku;	initCNF(sudoku, SUDOKU_VAR);
+	LITSHEET *ans = (LITSHEET *)malloc(sizeof(LITSHEET) * (1 + SUDOKU_VAR));
+	
+    char file[FILE_MAX];
+    int board[N][N] = {0};
+    int choice = 0, num_var = 0;
+
+    printf("What's your aim?\n");
+    printf("1. Build CNF from file and Solve\n");
+    printf("2. Play SUDOKU\n");
+    printf("3. Exit\n");
+    scanf("%d", &choice);
+
+    while(1){
+
+    	
+        switch (choice) {
+            case 1:
+            	printf("please input your filename(NOT need type .cnf):\n");
+            	scanf("%s",file);
+            	char inFile[FILE_MAX];
+            	strcpy(inFile, file);
+            	strcat(inFile, inSuf);
+            	
+            	ans = CNFparser(S,inFile,ans);
+            	if(!ans)	break;
+            	
+            	num_var = S.num_var;
+
+
+                if ( num_var ) {
+//					showCNF(S);
+
+					start = clock();
+					
+					bool t = DPLL(op_clau, S, ans/*, test*/);
+					
+					end = clock();
+					used_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+					
+					if( t ){
+						printf("\n\n");
+						check(ans, num_var);
+						/*check(test,ans, num_var);
+						fclose(test);*/
+						ans[0].ans = 1;
+					}else{
+						printf("ERROR! no solution\n");
+						ans[0].ans = 0;
+					}
+
+					printf("Duration:%.3f ms\n",used_time * 1000);
+				}
+                    
+                else 
+                    printf("Failed to parse CNF.\n");
+                    
+				Sleep(500);		//¼ä¸ô0.5s 
+				printf("\nSave as .res~~~\n");
+				
+				if(saveOutput(ans,num_var,file,used_time))	printf("RES file has been saved !\n");
+				else										printf("ERROR!File NOT be saved\n");
+				
+				printf("start delete and init ...\n");
+				
+				clearCNF(S, op_clau, ans) ;
+				for(int i = 0;i <= num_var; i++)	ans[i].ans = 0; 
+	
+                break;
+                
+            case 2:
+//            	int mode;
+//            	for(int i = 0;i <= SUDOKU_VAR;i++)	ans[i] = 0;
+//            	printf("Choose your game:1->SUDOKU 2->%%-SUDOKU 0->QUIT\n");
+//            	scanf("%d",&mode);
+//            	
+//            	if(!mode)	break;
+//            	
+//				fundConsCNF(sudoku);
+//				if(mode == 2)	percentConsCNF(sudoku);
+//				
+//				printf("sudoku num_clau = %d \n",sudoku.num_clau);
+//
+//				generate(/*test,*/ op_clau, sudoku, board, ans);
+//				clearCNF(sudoku, op_clau, ans);
+				
+				break;
+				
+            case 3:
+                printf("Exiting...\n");
+                free(ans);
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+        
+        printf("What's your next order?\n");
+        scanf("%d", &choice);
+    }
+
+
+
+    return 0;
+}
